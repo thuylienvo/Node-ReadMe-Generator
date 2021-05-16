a97ac1c2c633f600fbb5d56358d82c64fd2ad94e
Mimi Vo <mimzy414@gmail.com>
2021-05-16 17:06:18 -0500

    initiated npm

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+
     fs.writeFile(fileName, data, err => {
         if (err) throw err;
         console.log(fileName, data);
-    });
+    }
+)};
 
 // TODO: Create a function to initialize app
 
