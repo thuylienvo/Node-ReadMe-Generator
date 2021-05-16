c41acb5bf1dd64c988bbd7f4fc0c1ffc635d5190
Mimi Vo <mimzy414@gmail.com>
2021-05-16 16:52:48 -0500

    writeTofile function

---
+++ index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) throw err;
+        console.log(fileName, data);
+    });
+
+// TODO: Create a function to initialize app
+
+// Function call to initialize app
